68763301407d66eeb1c6df41a41e76c3f015a400
Martine1985 <genyagrib@gmail.com>
2020-08-20 11:20:25 +0300

    1

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Первая задача
 
-const firstRow = 'мама мыла раму';
+/*const firstRow = 'мама мыла раму';
 const secondRow = 'собака друг человека';
 let num_1 = 0;
 let num_2 = 0;
@@ -22,5 +22,46 @@ function getRow(firstRow, secondRow) {
 
 let a = getRow(firstRow,secondRow);
 console.log(a);
-
+*/
 // Надеюсь это лучше
+
+// Задача 2
+
+/*function formattedPhone(phone) {
+    
+   let str = '';
+   str = phone;
+   if(str.length == 12){
+   let a = str.slice(0, 2);
+   let b = str.slice(2, 5);
+   let c = str.slice(5, 8);
+   let d = str.slice(8, 10);
+   let e = str.slice(10, 12);
+   return `${a} (${b}) ${c}-${d}-${e}`;}
+   else return `Enter correct number`;
+}
+let a = formattedPhone('+712345890');
+console.log(a);*/
+
+function callNumber(){
+    let num = prompt('Enter phone number with first sign \+', '');
+    if(num[0] !== '+' || num.length < 12 || num.length > 12){
+        return alert('Try againe !') + callNumber();
+    }
+    else if(num.length == 12){
+        let a = num.slice(0, 2);
+        let b = num.slice(2, 5);
+        let c = num.slice(5, 8);
+        let d = num.slice(8, 10);
+        let e = num.slice(10, 12);
+   return alert(`${a} (${b}) ${c}-${d}-${e}`);}
+  
+}
+
+let a = callNumber();
+console.log(a);
+
+
+
+
+
